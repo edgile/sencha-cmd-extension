fb2d658e559086cf4b2c9c1d84b579aee99f9387
Diederik Zaaijer <diederik.zaaijer@ncia.nato.int>
2020-06-26 13:02:32 +0200

    Script improved

--- sencha-cmd-src/sencha-cmd.ps1
+++ sencha-cmd-src/sencha-cmd.ps1
@@ -1,32 +1,39 @@
 $downloadurl = "http://cdn.sencha.com/cmd/7.2.0.84/jre/SenchaCmd-7.2.0.84-windows-64bit.zip"
-
 $path = split-path $MyInvocation.MyCommand.Path
 
-Write-Output "Download SenchaCmd"
-$output = $path + "\SenchaCmd.zip"
+Write-Output "Command path: $path"
+Write-Output "Download SenchaCmd: $downloadurl"
 $start_time = Get-Date
+$output = $path + "\SenchaCmd.zip"
 
 (New-Object System.Net.WebClient).DownloadFile($downloadurl, $output)
 
 Write-Output "Time taken (Download): $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
 Write-Output "Extract SenchaCmd"
-$destination = $path + "\sencha-cmd"
 $start_time = Get-Date
 
 Add-Type -assembly "system.io.compression.filesystem"
-[io.compression.zipfile]::ExtractToDirectory($output, $destination)
+[io.compression.zipfile]::ExtractToDirectory($output, $path)
 
 Write-Output "Time taken (Unzip): $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
 Write-Output "Start installation of SenchaCmd"
 $start_time = Get-Date
-Start-Process -Wait -PassThru .\SenchaCmd-7.2.0.84-windows-64bit.exe -ArgumentList "-q -dir /"
+Start-Process -Wait -PassThru .\SenchaCmd-7.2.0.84-windows-64bit.exe -ArgumentList "-q -dir /sencha-cmd"
 
 Write-Output "Time taken (Installation): $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
 $javaOptions = "-Xms128m -Xmx2048m"
-$env:_JAVA_OPTIONS = $javaOptions
-Write-Host("##vso[task.setvariable variable=_JAVA_OPTIONS;]$javaOptions")
-
-$env:Path += $destination
+[Environment]::SetEnvironmentVariable(
+    "_JAVA_OPTIONS",
+    $javaOptions,
+    [EnvironmentVariableTarget]::User)
+	
+$env:Path += ";" + $path + "\sencha-cmd;"
+[Environment]::SetEnvironmentVariable(
+    "Path",
+    $env:Path,
+    [EnvironmentVariableTarget]::User)
+	
+Remove-Item $output
